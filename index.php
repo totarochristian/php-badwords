b16c522364cb50f0326aa5f298d9be5adad7de44
totarochristian <totaro.christian.18@gmail.com>
2023-05-03 13:43:10 +0200

    Initial setup

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Badwords</title>
+</head>
+<body>
+  <!-- Descrizione:
+  Creare una pagina PHP con un form che permetta di inviare due campi: un paragrafo ed una parola da censurare ad una seconda pagina php.
+  Gestire il tutto con i due file diversi.
+  Il primo file dovrà permettere all’utente di inserire i dati e inviare la richiesta al server.
+  Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
+  - stampare a schermo il paragrafo e la sua lunghezza
+  - stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare -->
+</body>
+</html>
